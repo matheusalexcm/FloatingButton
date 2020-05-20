61741689ca5db0be24406fe2041bd585af1288f1
Matheus <matheusalexandrecm@gmail.com>
2020-05-19 21:45:05 -0300

    Initial commit

---
+++ app/src/main/java/com/tads/floatingbutton/MainActivity.java
@@ -0,0 +1,28 @@
+package com.tads.floatingbutton;
+
+//Matheus Alexandre - 19/05/2020
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Parabens! Você acaba de ganhar 1 milhão de reais :D", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
